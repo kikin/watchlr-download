4c5a570251deb0dd9e7c9b8a066ed42ef7b09ea6
Sandesh Devaraju <scouredimage@gmail.com>
2011-07-08 18:38:34 -0400

    js location script

---
+++ jsloc.php
@@ -0,0 +1,26 @@
+<?php
+
+$CONFIG = 'version.json';
+
+$contents = file_get_contents($CONFIG);
+$decoded = json_decode($contents);
+
+$environ = $_COOKIE['_WENV'] != '' ? $_COOKIE['_WENV'] : 'prod';
+$selected = $decoded->{$environ};
+
+$version = $selected->{'version'};
+$base = $selected->{'base'};
+
+// URL scheme = http://$base/$version/watchlr-$version.min.js
+$location = $base . '/' . $version . '/watchlr-' . $version . '.min.js';
+
+$callback = $_GET['callback'];
+if ($callback != '') {
+    header('Content-type: application/javascript');
+    echo $callback . '({"js_url": "' . $location . '"});';
+} else {
+    header('Content-type: application/json');
+    echo '{"js_url": "' . $location . '"}';
+}
+
+?> 
